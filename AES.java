35ebb10dc41f995146dd13cb001d6be89a0826d0
Neelam Khada <71965791+zerokey-tech@users.noreply.github.com>
2022-05-09 12:07:15 +0530

    Update AES.java

--- AES.java
+++ AES.java
@@ -105,3 +105,23 @@ class AES {
 }
 
 //driver code
+public class Main {
+	 public static void main(String[] args)
+	 {
+	     // Create String variables
+	     String originalString = "Advanced Encryption Standard";
+	     
+	     // Call encryption method
+	     String encryptedString
+	         = AES.encrypt(originalString);
+	     
+	     // Call decryption method
+	     String decryptedString
+	         = AES.decrypt(encryptedString);
+
+	     // Print all strings
+	     System.out.println(originalString);
+	     System.out.println(encryptedString);
+	     System.out.println(decryptedString);
+	 }
+	}
